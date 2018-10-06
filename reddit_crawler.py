ad8bbace762d7c1e1cec9f5971b3b299aeb0c4ce
joaoocb <joaoocb@gmail.com>
2018-10-05 23:14:38 -0300

    Adding exception handling when inserting to DataBase

--- reddit_crawler.py
+++ reddit_crawler.py
@@ -1,6 +1,6 @@
 #! usr/bin/env python3
 import config
-import praw
+from praw import Reddit
 from pymongo import MongoClient
 import re
 
@@ -8,17 +8,19 @@ class RedditCrawler:
 
     def __init__(self, subreddit_name):
         # Create reddit and subreddit instances
-        self.reddit = praw.Reddit(client_id =     config.REDDIT_CONFIG["client_id"],
-                                  client_secret = config.REDDIT_CONFIG["client_secret"],
-                                  user_agent =    config.REDDIT_CONFIG["user_agent"],
-                                  username =      config.REDDIT_CONFIG["username"],
-                                  password =      config.REDDIT_CONFIG["password"])
+        self.reddit = Reddit(client_id =     config.REDDIT_CONFIG["client_id"],
+                             client_secret = config.REDDIT_CONFIG["client_secret"],
+                             user_agent =    config.REDDIT_CONFIG["user_agent"],
+                             username =      config.REDDIT_CONFIG["username"],
+                             password =      config.REDDIT_CONFIG["password"])
         self.subreddit = self.reddit.subreddit(subreddit_name)
 
         # Connect to data base
         try:
             self.mongo_client = MongoClient(config.DATABASE_CONFIG["mongodb_server"],
                                             config.DATABASE_CONFIG["mongodb_port"])
+            self.mongo_client.server_info()
+
             self.database = self.mongo_client["reddit_database"]
             self.colection = self.database["topics"]
         except:
@@ -36,7 +38,11 @@ class RedditCrawler:
                          "created":      submission.created,
                          "body":         submission.selftext}
 
-                self.colection.insert_one(topic)
+                try:
+                    result = self.colection.insert_one(topic).inserted_id
+                    print(result)
+                except:
+                    print("Failed to insert data to DataBase")
 
     def run(self):
         self.readTopics()
